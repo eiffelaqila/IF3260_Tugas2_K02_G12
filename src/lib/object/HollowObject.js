9fe0e71f8f18fc4c90381ef5faef92bd54e7730f
dawetmaster <andika.naufal48@gmail.com>
2023-03-22 11:47:21 +0700

    change to HollowObject, added verify function

--- src/lib/object/HollowObject.js
+++ src/lib/object/HollowObject.js
@@ -1,84 +1,31 @@
 /**
- * This is a base class for hollow objects used in this repository
- * @property {Object}
+ * Verify the JSON object that is valid for a hollow object (defined in sample.json).
+ * @param {Object} obj 
  */
-class HollowObject {
-    // Members of the base class
-
-    /** @type {number} Number of nodes used to draw hollow object */
-    #num_nodes;
-    /** @type {Array} List of nodes used to draw hollow object */
-    #nodes;
-    /** @type {number} Number of vertices/blocks used to draw hollow object */
-    #num_vertices;
-    /** @type {{faces: Array, color-type: String, color: Array}[]} List of vertices/blocks used to draw hollow object */
-    #vertices;
-
-    // Methods of the base class
-
-    /**
-     * Make a hollow object
-     */
-    constructor(obj) {
-        this.#nodes = obj.nodes ? obj.nodes : [];
-        this.#num_nodes = obj.nodes ? obj.nodes.length : 0;
-        this.#vertices = obj.vertices ? obj.vertices : [];
-        this.#num_vertices = obj.vertices ? obj.vertices.length : 0;
+function verifyHollowObject(obj) {
+    // Check if the object has nodes and vertices
+    if (!obj.hasOwnProperty('nodes')) return false;
+    if (!obj.hasOwnProperty('vertices')) return false;
+    // Check if nodes and vertices is an array
+    const nodes = obj.nodes;
+    const vertices = obj.vertices;
+    if (!Array.isArray(nodes)) return false;
+    if (!Array.isArray(vertices)) return false;
+    // Check if each element of nodes is an array
+    if (nodes.some(node => !Array.isArray(node) || node.length != 3 || node.some(el => typeof(el) != 'number'))) return false;
+    // Check if each element of vertices consists faces, color-type, and color
+    if (vertices.some(vertex => !vertex.hasOwnProperty('faces') || !vertex.hasOwnProperty('colors') || !vertex.hasOwnProperty('colorType'))) return false;
+    // Check every vertex in vertices is valid
+    for (const vertex of vertices) {
+        const faces = vertex.faces;
+        const colorType = vertex.colorType;
+        const colors = vertex.colors;
+        if (faces.some(face => !Array.isArray(face) || face.length <= 3 || face.some(el => typeof(el) != 'number'))) return false;
+        if (typeof(colorType) != 'string') return false;
+        if (colors.some(color => !Array.isArray(color) || color.length != 4 || color.some(el => typeof(el) != 'number'))) return false;
     }
-    /** 
-     * Get the number of nodes in the object
-     * 
-     * @return {number} Number of nodes in the object
-     */
-    get num_nodes() {
-        return this.#num_nodes;
-    }
-    /** 
-     * Get the list of nodes in the object
-     * 
-     * @return {Array} List of nodes in the object
-     */
-    get nodes() {
-        return this.#nodes;
-    }
-    /** 
-     * Get the number of vertices in the object
-     * 
-     * @return {number} Number of vertices in the object
-     */
-    get num_vertices() {
-        return this.#num_vertices;
-    }
-    /** 
-     * Get the list of vertices in the object
-     * 
-     * @return {Array} List of vertices in the object
-     */
-    get vertices() {
-        return this.#vertices;
-    }
-    /**
-     * Set the list of nodes in the object
-     * 
-     * @param {list} nodes List of nodes in the object
-     */
-    set nodes(nodes) {
-        if (Array.isArray(nodes)) {
-            this.#nodes = nodes;
-            this.#num_nodes = nodes.length;
-        }
-    }
-    /**
-     * Set the list of vertices in the object
-     * 
-     * @param {list} vertices List of vertices in the object
-     */
-    set vertices(vertices) {
-        if (Array.isArray(vertices)) {
-            this.#vertices = vertices;
-            this.#num_vertices = vertices.length;
-        }
-    }
+    // If the object passed verification above, then return true
+    return true;
 }
 
-export default HollowObject;
+export { verifyHollowObject };
